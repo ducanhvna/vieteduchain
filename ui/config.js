28daff23daedc605fc29ad1e28d2c4f00a1623de
DucAnh <ducanhvna@outlook.com>
2025-06-08 22:11:26 +0700

    update ui

--- ui/config.js
+++ ui/config.js
@@ -1,6 +1,6 @@
 // ui/config.js
 // Lấy API base URL từ biến môi trường hoặc fallback mặc định
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:8279';
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
 module.exports = {
   API_BASE_URL,
--- ui/next.config.js
+++ ui/next.config.js
@@ -1,18 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  async rewrites() {
-    return [
-      {
-        source: '/nodeinfo',
-        destination: 'http://localhost:8279/nodeinfo',
-      },
-      {
-        source: '/api/:path*',
-        destination: 'http://api:8000/api/:path*', // Sử dụng tên service Docker Compose
-      },
-    ];
-  },
 }
 
 module.exports = nextConfig;
